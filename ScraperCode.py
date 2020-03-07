f0b685a11fbf2b1ed9b2b09db4e55dbe14288ef7
boreybarflett <55071080+boreybarflett@users.noreply.github.com>
2020-03-07 10:17:33 -0500

    Add files via upload

--- ScraperCode.py
+++ ScraperCode.py
@@ -8,10 +8,11 @@ INPUTS:
     consumer_key, consumer_secret, access_token, access_token_secret: codes 
     telling twitter that we are authorized to access this data
     hashtag_phrase: the combination of hashtags to search for
-OUTPUTS:
-    none, simply save the tweet info to a spreadsheet
+
 """
-def search_for_hashtags(consumer_key, consumer_secret, access_token, access_token_secret, hashtag_phrase):
+def search_for_hashtags(consumer_key, consumer_secret, access_token, access_token_secret, hashtag_phrase, numberOfIterations):
+
+    
     
     #create authentication for accessing Twitter
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
@@ -30,19 +31,69 @@ def search_for_hashtags(consumer_key, consumer_secret, access_token, access_toke
 
         #write header row to spreadsheet
         #w.writerow(['timestamp', 'tweet_text', 'username', 'all_hashtags', 'followers_count'])
-    d = {"timestamp" : 0, "tweet_text" : 0, "username" : 0, "all_hashtags" : 0, "followers_count" : 0}
-    hashtags = {}
-    List = []
+    #d = {"timestamp" : 0, "tweet_text" : 0, "username" : 0, "all_hashtags" : 0, "followers_count" : 0}
+    k = 0
+    hashtagsSearchedFor = []
+    while k < numberOfIterations:
+        hashtags = {}
+        List = []
 
         #for each tweet matching our hashtags, write relevant info to the spreadsheet
-    for tweet in tweepy.Cursor(api.search, q=hashtag_phrase+' -filter:retweets', lang="en", tweet_mode='extended').items(100):
-        List.append([e['text'] for e in tweet._json['entities']['hashtags']])
-        List.append(tweet.user.screen_name.encode('utf-8'))
-        List.append(tweet.user.followers_count)
-        hashtags[hashtag_phrase] = List
+        for tweet in tweepy.Cursor(api.search, q=hashtag_phrase+' -filter:retweets', lang="en", tweet_mode='extended').items(100):
+            List.append([e['text'] for e in tweet._json['entities']['hashtags']])
+            List.append(tweet.user.screen_name.encode('utf-8'))
+            List.append(tweet.user.followers_count)
+            hashtags[hashtag_phrase] = List
+
+        results = {}
+    
+        for i in range(0, len(List), 3):
+            for j in List[i]:
+                if j in results:
+                    results[j] = results[j] +1
+                else:
+                    results[j] = 1
+        sortedResults = sorted(results.items(),key=lambda x: x[1], reverse=True)
+        print(results)
+       
+        
+        
+
+        relatedhashtag = sortedResults[1][0]
+        
+
+        print(sortedResults)
+        print(sortedResults[1][0])
+        print(hashtagsSearchedFor)
+        for relatedhashtag in hashtagsSearchedFor:
+            if not relatedhashtag in hashtagsSearchedFor:
+                relatedhashtag = sortedResults[1][0]
+                hashtag_phrase = relatedhashtag
+                hashtagsSearchedFor.append(sortedResults[1][0])
+                
+            else:
+                coolindex = 0
+                while coolindex > 10:
+                    relatedhashtag = sortedResults[1+coolindex][0]
+                    for relatedhashtag in hashtagsSearchedFor:
+                        if not relatedhashtag in hashtagsSearchedFor:
+                            hashtag_phrase = sortedResults[1+coolindex]
+                            hashtagsSearchedFor.append(sortedResults[1+coolindex][0])
+                            
+                    coolindex += 1
+                
+                
+
+            
+            
+            
+        k += 1
+        
+        
+     
                     
             #d([tweet.created_at, tweet.full_text.replace('\n',' ').encode('utf-8'), tweet.user.screen_name.encode('utf-8'), [e['text'] for e in tweet._json['entities']['hashtags']], tweet.user.followers_count])
-    return hashtags
+    return list
             
             #w.writerow([tweet.created_at, tweet.full_text.replace('\n',' ').encode('utf-8'), tweet.user.screen_name.encode('utf-8'), [e['text'] for e in tweet._json['entities']['hashtags']], tweet.user.followers_count])
 consumer_key = "ghybXiUPe9ixRZNGJKxQsOgv8"
@@ -54,6 +105,7 @@ hashtag_phrase = input('Hashtag Phrase ')
 
 def main():
 
-    h = search_for_hashtags(consumer_key, consumer_secret, access_token, access_token_secret, hashtag_phrase)
-    print(h)
+    h = search_for_hashtags(consumer_key, consumer_secret, access_token, access_token_secret, hashtag_phrase, 10)
+    
+    
 main()
