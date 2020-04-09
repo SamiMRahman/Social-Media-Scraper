5fafdb95d1067ece1db9018d5cadad28f7b242d4
boreybarflett <55071080+boreybarflett@users.noreply.github.com>
2020-04-08 20:46:04 -0400

    Add files via upload

--- ScraperCode.py
+++ ScraperCode.py
@@ -8,7 +8,6 @@ INPUTS:
     consumer_key, consumer_secret, access_token, access_token_secret: codes 
     telling twitter that we are authorized to access this data
     hashtag_phrase: the combination of hashtags to search for
-
 """
 def search_for_hashtags(consumer_key, consumer_secret, access_token, access_token_secret, hashtag_phrase, numberOfIterations):
 
@@ -22,28 +21,34 @@ def search_for_hashtags(consumer_key, consumer_secret, access_token, access_toke
     api = tweepy.API(auth)
     
     #get the name of the spreadsheet we will write to
-    #fname = '_'.join(re.findall(r"#(\w+)", hashtag_phrase))
+    fname = '_'.join(re.findall(r"#(\w+)", hashtag_phrase))
 
     #open the spreadsheet we will write to
-    #with open(hashtag_phrase + '.csv', 'w') as file:
+    
 
-        #w = csv.writer(file)
+        
 
         #write header row to spreadsheet
-        #w.writerow(['timestamp', 'tweet_text', 'username', 'all_hashtags', 'followers_count'])
+        
     #d = {"timestamp" : 0, "tweet_text" : 0, "username" : 0, "all_hashtags" : 0, "followers_count" : 0}
     k = 0
-    hashtagsSearchedFor = []
+    hashtagsSearchedFor = [hashtag_phrase]
+    
     while k < numberOfIterations:
-        hashtags = {}
-        List = []
+        fname = '_'.join(re.findall(r"#(\w+)", hashtag_phrase))
+        with open(hashtag_phrase + '.csv', 'w') as file:
+            w = csv.writer(file)
+            w.writerow(['tweet_text', 'username', 'all_hashtags', 'followers_count','profile_link'])
+            hashtags = {}
+            List = []
 
         #for each tweet matching our hashtags, write relevant info to the spreadsheet
-        for tweet in tweepy.Cursor(api.search, q=hashtag_phrase+' -filter:retweets', lang="en", tweet_mode='extended').items(100):
-            List.append([e['text'] for e in tweet._json['entities']['hashtags']])
-            List.append(tweet.user.screen_name.encode('utf-8'))
-            List.append(tweet.user.followers_count)
-            hashtags[hashtag_phrase] = List
+            for tweet in tweepy.Cursor(api.search, q=hashtag_phrase+' -filter:retweets', lang="en", tweet_mode='extended').items(100):
+                w.writerow([tweet.full_text.replace('\n',' ').encode('utf-8'), tweet.user.screen_name.replace('b', '', 1).replace('\'',''), [e['text'] for e in tweet._json['entities']['hashtags']], tweet.user.followers_count,'twitter.com/'+tweet.user.screen_name.replace('b', '', 1).replace('\'','')])
+                List.append([e['text'] for e in tweet._json['entities']['hashtags']])
+                List.append(tweet.user.screen_name.encode('utf-8'))
+                List.append(tweet.user.followers_count)
+                hashtags[hashtag_phrase] = List
 
         results = {}
     
@@ -59,29 +64,26 @@ def search_for_hashtags(consumer_key, consumer_secret, access_token, access_toke
         
         
 
-        relatedhashtag = sortedResults[1][0]
+        
         
 
         print(sortedResults)
-        print(sortedResults[1][0])
+        
         print(hashtagsSearchedFor)
-        for relatedhashtag in hashtagsSearchedFor:
-            if not relatedhashtag in hashtagsSearchedFor:
-                relatedhashtag = sortedResults[1][0]
-                hashtag_phrase = relatedhashtag
-                hashtagsSearchedFor.append(sortedResults[1][0])
-                
-            else:
-                coolindex = 0
-                while coolindex > 10:
-                    relatedhashtag = sortedResults[1+coolindex][0]
-                    for relatedhashtag in hashtagsSearchedFor:
-                        if not relatedhashtag in hashtagsSearchedFor:
-                            hashtag_phrase = sortedResults[1+coolindex]
-                            hashtagsSearchedFor.append(sortedResults[1+coolindex][0])
-                            
-                    coolindex += 1
-                
+        
+        if sortedResults[1][0].lower() not in hashtagsSearchedFor:
+            hashtagsSearchedFor.append(sortedResults[1][0])
+            relatedhashtag = sortedResults[1][0]
+        else:
+            for a in range(len(sortedResults)):
+                if sortedResults[a][0].lower() in hashtagsSearchedFor:
+                    continue
+                else:
+                    hashtagsSearchedFor.append(sortedResults[a][0])
+                    relatedhashtag = sortedResults[a][0]
+                    break
+        hashtag_phrase = relatedhashtag        
+        print(relatedhashtag)
                 
 
             
@@ -100,12 +102,13 @@ consumer_key = "ghybXiUPe9ixRZNGJKxQsOgv8"
 consumer_secret = "UEzdwWgy0IOZOZ1Qk5nuvmquDQ1wkyiaOaZAmrRbMGK1fLeVt3"
 access_token = "4497707541-j5FJ6vDwrCws68RaKIaynP4ZhX7sfdQDF5o0E3C"
 access_token_secret = "i0aGublGJ13ipAZOuhmQ8tG7Gyr3UKDIlkpl2ukMjEfdd"
-hashtag_phrase = input('Hashtag Phrase ')
+hashtag_phrase = input('Hashtag Phrase: ')
+anything = int(input('Number of Searches: '))
 
 
 def main():
 
-    h = search_for_hashtags(consumer_key, consumer_secret, access_token, access_token_secret, hashtag_phrase, 10)
+    h = search_for_hashtags(consumer_key, consumer_secret, access_token, access_token_secret, hashtag_phrase, anything)
     
     
 main()
