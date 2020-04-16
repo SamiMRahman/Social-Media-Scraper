9dd1adaadfee33f07964a43b987569c7690d0ea9
boreybarflett <55071080+boreybarflett@users.noreply.github.com>
2020-04-16 17:04:45 -0400

    Add files via upload

--- ScraperCode.py
+++ ScraperCode.py
@@ -33,41 +33,73 @@ def search_for_hashtags(consumer_key, consumer_secret, access_token, access_toke
     #k = 0
     #hashtagsSearchedFor = [hashtag_phrase]
     
-    #while k < numberOfIterations:
+    #while k < numberOfIterations: 
     fname = '_'.join(re.findall(r"#(\w+)", hashtag_phrase))
     with open(hashtag_phrase + '.csv', 'w',encoding ='utf-8') as file:
         w = csv.writer(file)
         w.writerow(['tweet_text', 'username', 'all_hashtags', 'followers_count','profile_link'])
         hashtags = {}
+        queries = 10
         List = []
+        for i in range(queries):
+            List.append([])
+##            print(List)
+        
 
         #for each tweet matching our hashtags, write relevant info to the spreadsheet
         foundUsers = []
+        foundHashtags = []
+        foundFollowerCount = []
+        
 
-        for tweet in tweepy.Cursor(api.search, q=hashtag_phrase+' -filter:retweets', lang="en", tweet_mode='extended').items(100):
-            List.append([e['text'] for e in tweet._json['entities']['hashtags']])
-            List.append(tweet.user.screen_name)
-            List.append(tweet.user.followers_count)
-            hashtags[hashtag_phrase] = List
+        for tweet in tweepy.Cursor(api.search, q=hashtag_phrase+' -filter:retweets', lang="en", tweet_mode='extended').items(5):
             
+            foundUsers.append(tweet.user.screen_name)
+            foundHashtags.append([e['text'] for e in tweet._json['entities']['hashtags']])
+            foundFollowerCount.append(tweet.user.followers_count)
 
-        results = {}
-    
-        for i in range(0, len(List), 3):
-            for j in List[i]:
-                if j in results:
-                    results[j] = results[j] +1
-                else:
-                    results[j] = 1
-        sortedResults = sorted(results.items(),key=lambda x: x[1], reverse=True)
-        for k in range(len(sortedResults)):
-            numberOfRelatedHashtags = sortedResults
+        print(foundUsers)
+        print(foundHashtags)
+        print(foundFollowerCount)
+
+        for k in range(len(foundUsers)):
+            List[k].append(foundUsers[k])
+            List[k].append(foundHashtags[k])
+            List[k].append(foundFollowerCount[k])
+        print(List)
+            
+            
+##            i=+1
+##            
+##            
+##            
+##            List[i].append([e['text'] for e in tweet._json['entities']['hashtags']])
+##            List[i].append(tweet.user.screen_name)
+##            List[i].append(tweet.user.followers_count)
+                    
+                    
+                
+            #hashtags[hashtag_phrase] = List
+            
+##        print(List)
+##        results = {}
+##    
+##        for i in range(0, len(List), 3):
+##            for j in range(0, len(List[i]),3):
+##                for k in List[i][j]:
+##                    if k in results:
+##                        results[k] = results[k] + 1
+##                    else:
+##                        results[k] = 1
+##        sortedResults = sorted(results.items(),key=lambda x: x[1], reverse=True)
+##        for k in range(len(sortedResults)):
+##            numberOfRelatedHashtags = sortedResults
 ##            
 ##            
 ##
 ##        
 ##            
-##        for tweet in tweepy.Cursor(api.search, q=hashtag_phrase+' -filter:retweets', lang="en", tweet_mode='extended').items(100):
+##        for tweet in tweepy.Cursor(api.search, q=hashtag_phrase+' -filter:retweets', lang="en", tweet_mode='extended').items(3):
 ##            if tweet.user.screen_name.replace('b', '', 1).replace('\'','') not in foundUsers and numberOfRelatedHashtags == k:
 ##                
 ##                w.writerow([tweet.full_text.replace('\n',' ').replace('b', '', 1), tweet.user.screen_name.replace('b', '', 1).replace('\'',''), [e['text'] for e in tweet._json['entities']['hashtags']], tweet.user.followers_count,'twitter.com/'+tweet.user.screen_name.replace('b', '', 1).replace('\'','')])
